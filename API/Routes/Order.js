5a649730758718e7f9c3195e721989177378475e
Eduardo R Vieira <eduardovieirayt@gmail.com>
2023-11-18 18:04:44 -0300

    bla

--- API/Routes/Order.js
+++ API/Routes/Order.js
@@ -9,7 +9,15 @@ router.get('/', (req, res, next) => {
 
 router.post('/', (req, res, next) => {
 
-    res.status(201).json({message: 'Order was created'});
+    const order = {
+        productId: req.body.productId,
+        quantity: req.body.quantity
+    };
+
+    res.status(201).json({
+        message: 'Order was created',
+        order: order
+    });
 
 });
 
@@ -29,5 +37,5 @@ router.delete('/:orderId', (req, res, next) => {
 
 });
 
-
+// testing the nodemon
 module.exports = router;
--- app.js
+++ app.js
@@ -1,11 +1,50 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-const productRouter = require('./API/Routes/Products');
-const orderRouter = require('./API/Routes/Order');
+const productRouter = require('./API/Routes/products');
+const orderRouter = require('./API/Routes/order');
+
+mongoose.connect('mongodb+srv://EduardoVieira:'+ process.env.MONGO_ATLAS_PW +'@cluster0.kst562m.mongodb.net/?retryWrites=true&w=majority');
+
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Headers', 
+        'Origin, X-Requested-With, Content-Type'
+    );
+
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, PATCH, DELETE');
+        return res.status(200).json({});
+    }
+    next();
+});
+
 
 
 app.use('/products', productRouter);
 app.use('/orders', orderRouter);
+
+app.use((req, res, next) => {
+    const error = new Error("not found");
+    error.status = 404;
+    next(error);
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+})
 
 module.exports = app;
