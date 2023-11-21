89eab44b7b1fac1a98cd51b3d1308890af6503c1
Eduardo R Vieira <eduardovieirayt@gmail.com>
2023-11-21 11:06:24 -0300

    create

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const http = require('http');
 const app = require('./app');
 
+const hostname = '0.0.0.0';
 const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
 
-server.listen(port);
+server.listen(port, hostname);
 
